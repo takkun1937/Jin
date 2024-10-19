9d2b29aeec9a06fc14ec4016981ee3ea568e0848
takkun1937 <takujin3811@gmail.com>
2024-10-19 20:03:42 +0000

    update: asyncの削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import ContentListContainer from '@/features/contents/read/components/ContentListContainer';
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className='px-8 py-6'>
       <ContentListContainer isMyContent={false} />
